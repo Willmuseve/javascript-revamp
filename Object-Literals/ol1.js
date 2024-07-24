23884fd8cfd1eedf0e0a3d5a23ddadff0be3c848
Willmuseve <willmuseve@gmail.com>
2024-07-24 06:17:13 +0300

    Object - Literals - 1

---
+++ Object-Literals/ol1.js
@@ -0,0 +1,10 @@
+//object literals 1
+
+let user = {
+    name : "Michael Wilson",
+    number: "0798256760",
+    email: "willmuseve@gmail.com",
+    designation: "developer"
+}
+
+console.log(user.name)
