d61cea58afbcb4196c3b950c3ffa9d980325735e
Willmuseve <willmuseve@gmail.com>
2024-07-25 06:48:56 +0300

    math f

--- Object-Literals/math.js
+++ Object-Literals/math.js
@@ -7,3 +7,7 @@ console.log(Math.PI)
 console.log(Math.round(num))
 console.log(Math.ceil(num))
 console.log(Math.floor(num))
+
+//random
+let random = Math.random() * 1000
+console.log(Math.round(random))
