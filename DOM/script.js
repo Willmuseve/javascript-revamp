74a23b2202f8ca246460e84e54736809a8cd0d4e
Willmuseve <willmuseve@gmail.com>
2024-08-01 05:57:04 +0300

    footer html

--- DOM/script.js
+++ DOM/script.js
@@ -3,3 +3,6 @@ let description = document.querySelectorAll('main p');
 description.forEach((paragraph) =>{
     console.log(paragraph)
 })
+
+let update = document.querySelector(".sub-heading")
+update.innerHTML = "New Subheading"
