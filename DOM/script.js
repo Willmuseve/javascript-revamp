08170a23655aadb96ccfd7027327f0df1194bad5
Willmuseve <willmuseve@gmail.com>
2024-08-01 06:21:31 +0300

    innerhtml

--- DOM/script.js
+++ DOM/script.js
@@ -6,3 +6,10 @@ description.forEach((paragraph) =>{
 
 let update = document.querySelector(".sub-heading")
 update.innerHTML = "New Subheading"
+
+
+let liH = document.querySelectorAll("ul.top-class li")
+console.log(liH)
+liH.forEach((element) =>{
+    element.innertext += "Menu"
+})
